6a7860029284578e6c4a8573755a6d62412f3430
josh1502-bit <josnelgabriel@gmail.com>
2021-06-30 13:00:47 -0400

    2 files modified

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-const btn = document.getElementById('btn');
+var btn = document.getElementById('btn');
+var copyText = document.getElementById('myInput');
+var color = '';
 
 var re = () => {
   var r = document.getElementById('r-color').value;
   var g = document.getElementById('g-color').value;
   var b = document.getElementById('b-color').value;
-  var color = 'rgb(' + r + ', ' + g + ', ' + b + ')';
+  color = 'rgb(' + r + ', ' + g + ', ' + b + ')';
   document.body.style.backgroundColor = color;
   btn.innerHTML = color;
+  copyText.value = color;
+};
+
+var copy = () => {
+  /* Get the text field */
+  // var copyText = document.getElementById('btn');
+
+  /* Select the text field */
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+
+  /* Copy the text inside the text field */
+  document.execCommand('copy');
+
+  /* Alert the copied text */
+  alert('Copied the text: ' + copyText.value);
 };
 
 re();

82447864f8cce57fdf98f198cd99cae0f033d09f
josh1502-bit <josnelgabriel@gmail.com>
2021-06-30 11:57:02 -0400

    3 files modified, 1 deleted

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log('hello!')
+var r = document.getElementById('r-color');
+var g = document.getElementById('g-color');
+var b = document.getElementById('b-color');
+
+document.body.style.backgroundColor =
+  'rgb(' + 120 + ', ' + 30 + ', ' + 100 + ')';
